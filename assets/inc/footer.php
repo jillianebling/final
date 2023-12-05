21acf7bb20711166982005949c37d05b0fe5b2c8
jee6650 <jee6650@g.rit.edu>
2023-12-05 16:12:11 -0500

    almost done setting up

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,5 +1,8 @@
 <footer>
+    <br/>
+    <br/>
     <link href="assets/css/footer.css" rel="stylesheet" type="text/css">
-    <p>Copyright Jillian Ebling 2023</p>
-    <p>RIT - Web and Mobile II</p>
+    <h6>Jillian Ebling 2023</h6>
+    <h6>RIT - Web and Mobile II</h6>
+    <br/>
 </footer>
