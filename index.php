ebcd5ea728676c0f965d48d54ed2629032340689
jee6650 <jee6650@g.rit.edu>
2023-12-09 22:01:34 -0500

    map embed added

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
             Populating much of the St. Lawrence River, the 1000 Islands feature many gorgeous views and a stunning look into history at the Boldt and Singer castles. Visitors with diving licenses are encouraged to dive into one of the many historical shipwreck sites along the river. 
             Not into adventure? Don’t worry! Alexandria Bay is just a stone’s throw away from the 1000 Island State Park. A-Bay provides leisurely lakefront dining, boutique shopping, boat tours, and more!
             </p>
-            <img class="map" src="assets/media/images/colormap.jpg" alt="Map of Wellesley Island and Surrounding Area">
-        </div>    
-        <img class="map2" src="assets/media/images/colormap.jpg" alt="Map of Wellesley Island and Surrounding Area">
+            <iframe class='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45667.55387368979!2d-75.9884135!3d44.32593985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccd478564e99d9d%3A0xa698248823c50752!2sWellesley%20Island!5e0!3m2!1sen!2sus!4v1702176020489!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>        
+            <iframe class='map2' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45667.55387368979!2d-75.9884135!3d44.32593985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccd478564e99d9d%3A0xa698248823c50752!2sWellesley%20Island!5e0!3m2!1sen!2sus!4v1702176020489!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>        
+
     </div>
     <?php
         include("assets/inc/footer.php");
